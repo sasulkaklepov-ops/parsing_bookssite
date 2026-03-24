f1923ba0529bed365e608835ef183c2b91a8b209
Sanya_Kl_2010_ <sasulkaklepov@gmail.com>
2026-03-24 20:12:58 +0300

    Update pars.py

--- pars.py
+++ pars.py
@@ -21,7 +21,6 @@ for book in books:
     t=book.find('a', title=True)
     title=t.get('title')
     pr=book.find("p", class_="price_color").text
-    #print(title, ": ", pr)
     ws[f"A{a}"]=title
     ws[f"B{a}"]=pr
     a+=1
